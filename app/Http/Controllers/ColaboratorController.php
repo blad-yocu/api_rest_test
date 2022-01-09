8b23ba81354e27ca58882677683a7930e3221305
Abraham <abraham.vlak@gmail.com>
2022-01-09 16:24:41 -0600

    [feat]Creacion de dos rutas api, controlador y variables de entorno

---
+++ app/Http/Controllers/ColaboratorController.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use NunoMaduro\Collision\Contracts\Writer;
+use PHPUnit\Util\Json;
+
+class ColaboratorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    protected $api_key;
+    protected $api_private;
+    protected $ts;
+    public $hash;
+
+    public function __construct()
+    {
+        $this->api_key = env('API_KEY_PUBLIC');
+        $this->api_private = env('API_KEY_PRIVATE');
+        $this->ts = env('TS');
+        $this->hash = md5($this->ts . $this->api_private . $this->api_key);
+    }
+    public function getApi($url)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = json_decode(curl_exec($ch));
+        curl_close($ch);
+        return $response;
+    }
+    public function index($character = '', $type = 'name')
+    {
+        try {
+            if (empty($character)) {
+                $url = 'http://gateway.marvel.com/v1/public/characters?ts=' . $this->ts . '&apikey=' . $this->api_key . '&hash=' . $this->hash . '';
+                $response = $this->getApi($url);
+                return $response;
+            } else {
+
+
+                $character = str_replace(" ", "%20", trim($character));
+                $url = 'http://gateway.marvel.com/v1/public/characters?ts=' . $this->ts . '&apikey=' . $this->api_key . '&hash=' . $this->hash . '&' . $type . '=' . ($character);
+
+                $response = $this->getApi($url);
+                if ($response->code == 200) {
+
+                    $comics = [];
+                    //validar si hay resultados
+                    empty($response->data->results) ? $response = $this->getApi($url) : '';
+                    if (!empty($response->data->results)) {
+                        foreach ($response->data->results as $item) {
+                            $comics[] = $item->comics;
+                        }
+
+                        $urls_comics = collect($comics)->pluck('collectionURI');
+
+                        $url_comic = $urls_comics->first();
+                        $url_comic = $url_comic . '?ts=' . $this->ts . '&apikey=' . $this->api_key . '&hash=' . $this->hash;
+                        //obenemos los comics con el id del persinaje
+                        $list_comics = $this->getApi($url_comic);
+                        //en una coleccion filtramos todos los items para dividir los editores y escritores
+                        $list_creators = collect($list_comics->data->results)->pluck('creators.items')->collapse();
+                        $editors = [];
+                        $writers = [];
+                        foreach ($list_creators as $creator) {
+                            if ($creator->role == "writer") {
+                                $writers[] = $creator->name;
+                            }
+                            if ($creator->role == "editor") {
+                                $editors[] = $creator->name;
+                            }
+                        }
+                        $colaborators = [
+                            'editors' => $editors,
+                            'writers' => $writers
+                        ];
+                        return $colaborators;
+                    } else {
+                        //encaso de que no hay resultados
+                        return response()->json(['message' => 'No se encontro resultados '], 404);
+                    }
+                } else {
+                    return response()->json(['message' => 'Recurso no encontrado'], 404);
+                }
+            }
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json(['message' => 'Error en la peticion rest, validar parametros', 'code' => 409], 409);
+        }
+    }
+
+    public function characters($character = '', $type = 'name')
+    {
+        try {
+            if (empty($character)) {
+                $url = 'http://gateway.marvel.com/v1/public/characters?ts=' . $this->ts . '&apikey=' . $this->api_key . '&hash=' . $this->hash . '';
+                $response = $this->getApi($url);
+                return $response;
+            } else {
+
+
+                $character = str_replace(" ", "%20", trim($character));
+                $url = 'http://gateway.marvel.com/v1/public/characters?ts=' . $this->ts . '&apikey=' . $this->api_key . '&hash=' . $this->hash . '&' . $type . '=' . ($character);
+
+                $response = $this->getApi($url);
+                if ($response->code == 200) {
+
+                    $comics = [];
+                    //validar si hay resultados
+                    empty($response->data->results) ? $response = $this->getApi($url) : '';
+                    if (!empty($response->data->results)) {
+                        foreach ($response->data->results as $item) {
+                            $comics[] = $item->comics;
+                        }
+
+                        $urls_comics = collect($comics)->pluck('collectionURI');
+
+                        $url_comic = $urls_comics->first();
+                        $url_comic = $url_comic . '?ts=' . $this->ts . '&apikey=' . $this->api_key . '&hash=' . $this->hash;
+                        //obenemos los comics con el id del persinaje
+                        $list_comics = $this->getApi($url_comic);
+                        $list_comics = collect($list_comics->data->results);
+                        $list_characters = $list_comics->pluck('characters.items')->collapse();
+                        $list_characters = $list_characters->unique('resourceURI');
+                        $object_response = [];
+                        $characters = [];
+                        foreach ($list_characters as $character) {
+                            $url = $character->resourceURI . '?ts=' . $this->ts . '&apikey=' . $this->api_key . '&hash=' . $this->hash;
+                            $lists_character_comics = $this->getApi($url);
+                            $list_comics_character = collect($lists_character_comics->data->results)->pluck('comics.items')->collapse();
+                            $array_comics = $list_comics_character->pluck('name')->toArray();
+                            $characters = [
+                                'character' => $character->name,
+                                'Comics' => $array_comics
+                            ];
+                            $object_response[] = $characters;
+                        }
+
+                        return $object_response;
+                        //en una coleccion filtramos todos los items para dividir los editores y escritores
+                        //$list_creators = collect($list_comics->data->results)->pluck('creators.items')->collapse();
+                    } else {
+                        //encaso de que no hay resultados
+                        return response()->json(['message' => 'No se encontro resultados '], 404);
+                    }
+                } else {
+                    return response()->json(['message' => 'Recurso no encontrado'], 404);
+                }
+            }
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+    }
+}
